ee0f0e14dde768c81160ee0fad4b403ca08f4ec9
Skomorac <admir.skomorac@outlook.com>
2024-03-18 13:59:14 +0100

    final version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const todayDate = new Date().toDateString();
+
+  // Function to calculate the updated date based on the count
+  function calculateUpdatedDate() {
+    const currentDate = new Date();
+    const updatedDate = new Date(
+      currentDate.setDate(currentDate.getDate() + count)
+    );
+    return updatedDate.toDateString();
+  }
+
+  function plusCount() {
+    setCount((currentCount) => currentCount + step);
+  }
+
+  function minusCount() {
+    setCount((currentCount) => currentCount - step);
+  }
+
+  function plusStep() {
+    setStep((currentStep) => currentStep + 1);
+  }
+
+  function minusStep() {
+    setStep((currentStep) => currentStep - 1);
+  }
+
+  return (
+    <div>
+      <div className="counter-container">
+        <button onClick={minusStep}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={plusStep}>+</button>
+      </div>
+
+      <div className="counter-container">
+        <button onClick={minusCount}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={plusCount}>+</button>
+      </div>
+      <p>
+        {count === 0
+          ? `Today is ${todayDate}`
+          : count > 0
+          ? `${count} days from today is ${calculateUpdatedDate()}`
+          : `${Math.abs(count)} days ago was ${calculateUpdatedDate()}`}
+      </p>
     </div>
   );
 }
